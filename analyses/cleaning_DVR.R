03e61a4b36eb300d341061aaddb13502279b94d1
cchambe12 <cchamberlain@g.harvard.edu>
2018-08-31 09:58:01 -0400

    more models and cleaning!

--- analyses/cleaning_DVR.R
+++ analyses/cleaning_DVR.R
@@ -15,6 +15,8 @@ library(lubridate)
 library(weathermetrics)
 library(geosphere)
 library(rstanarm)
+library(broom)
+library(brms)
 
 # Set Working Directory
 setwd("~/Documents/git/CGtraits/analyses")
@@ -45,17 +47,36 @@ drisk<-dx%>%dplyr::select(Ind, Plot, bb, lo, species)
 drisk<-drisk[!(is.na(drisk$bb) & is.na(drisk$lo)),]
 
 bb<-drisk[!is.na(drisk$bb),]
-bb$budburst<-ave(bb$bb, bb$Ind, bb$Plot, FUN=first)
+bb$budburst<-ave(bb$bb, bb$Ind, bb$Plot, FUN=min)
 bb<-subset(bb, select=c("Ind", "Plot", "budburst"))
 bb<-bb[!duplicated(bb),]
 lo<-drisk[!is.na(drisk$lo),]
-lo$leafout<- ave(lo$lo, lo$Ind, lo$Plot, FUN=first) 
+lo$leafout<- ave(lo$lo, lo$Ind, lo$Plot, FUN=min) 
 lo<-subset(lo, select=c("Ind", "Plot", "leafout"))
 lo<-lo[!duplicated(lo),]
 
 dvr<-full_join(bb, lo)
 dvr$risk<-dvr$leafout-dvr$budburst 
 
+dvr<-na.omit(dvr)
+### Starting here, need to check code... ####
+dvr<- dvr[order(dvr$Ind, dvr$risk), ]
+dvr$Ind<-paste(dvr$Ind, dvr$Plot, sep="_")
+dvr$ind.risk<-paste(dvr$Ind, dvr$risk, sep=",")
+days.btw <- Map(seq, dvr$budburst, dvr$leafout, by = 1)
+
+dxx <- data.frame(ind.risk = rep.int(dvr$ind.risk, vapply(days.btw, length, 1L)), 
+                  doy = do.call(c, days.btw))
+
+dxx$Ind<-gsub(",.*", "", dxx$ind.risk)
+dxx$risk<-gsub(".*,", "", dxx$ind.risk)
+dxx<-dplyr::select(dxx, -ind.risk)
+dxx$budburst<-ave(dxx$doy, dxx$Ind, FUN=min)
+dxx$leafout<-ave(dxx$doy, dxx$Ind, FUN=max)
+
+dvr<-dplyr::select(dvr, Ind, Plot)
+
+dvr<-full_join(dxx, dvr)
 
 ### Let's add in traits data now!### 
 traits<-read.csv("output/clean_traits.csv", header=TRUE)
@@ -63,17 +84,16 @@ traits$d.index<-traits$perim/(2*sqrt(traits$area*pi))
 traits$Ind<-paste(traits$species, traits$site, traits$ind, traits$plot, sep="_")
 traits$d.index<-ave(traits$d.index, traits$Ind)
 
-dvr$Ind<-paste(dvr$Ind, dvr$Plot, sep="_")
-
 test<-full_join(dvr, traits)
 test$sla<-test$area/test$dr.wt
 
-test<-subset(test, select=c("risk", "Ind", "budburst", "d.index", "species", "area", "sla"))
+test<-subset(test, select=c("doy", "risk", "Ind", "budburst", "d.index", "species", "area", "sla"))
 test<-test[!duplicated(test),]
 test<-test[!(test$risk<0),]
 test<-na.omit(test)
 test$area<-ave(test$area, test$Ind)
 test$sla<-ave(test$sla, test$Ind)
+test$d.index<-ave(test$d.index, test$Ind)
 test<-test[!duplicated(test),]
 
 
@@ -131,12 +151,13 @@ ccx$photo<-daylength(ccx$lat, ccx$dat)
 ccx$chill<-ave(ccx$chill, ccx$doy, FUN=max)
 ccx<-subset(ccx, ccx$year==2018)
 
-ccx<-subset(ccx, select=c(doy, chill, gdd, photo))
+ccx<-subset(ccx, select=c(doy, chill, tmean, photo, tmin))
 ccx<-ccx[!duplicated(ccx),]
 
 test$chill<-NA
 test$force<-NA
 test$photo<-NA
+test$tmin<-NA
 
 for(i in c(1:nrow(test))){
   for(j in c(1:nrow(ccx)))
@@ -144,26 +165,52 @@ for(i in c(1:nrow(test))){
 }
 for(i in c(1:nrow(test))){
   for(j in c(1:nrow(ccx)))
-    test$force[i]<-ifelse(test$budburst[i]==ccx$doy[j], ccx$gdd[j], test$force[i])
+    test$force[i]<-ifelse(test$doy[i]==ccx$doy[j], ccx$tmean[j], test$force[i])
 }
 for(i in c(1:nrow(test))){
   for(j in c(1:nrow(ccx)))
     test$photo[i]<-ifelse(test$budburst[i]==ccx$doy[j], ccx$photo[j], test$photo[i])
 }
+for(i in c(1:nrow(test))){
+  for(j in c(1:nrow(ccx)))
+    test$tmin[i]<-ifelse(test$budburst[i]==ccx$doy[j], ccx$tmin[j], test$tmin[i])
+}
 
+test$force<-ave(test$force, test$Ind)
+test$photo<-ave(test$photo, test$Ind)
+test$frz<-ifelse(test$tmin<=-2.2, 1, 0)
+test$frz<-ave(test$frz, test$Ind, FUN=max)
+test<-dplyr::select(test, -doy)
+test<-test[!duplicated(test),]
 
 test$sm.sla<-test$sla/10
 test$sm.chill<-test$chill/100
-test$sm.gdd<-test$force/10
+test$risk<-as.numeric(test$risk)
+test$site<-substr(test$Ind, 8,9)
+test$numinds<-ave(test$Ind, test$species, test$site, FUN=length)
 
-fit<-stan_glmer(risk~d.index+budburst+sm.chill+sm.gdd+photo+(d.index+budburst+sm.chill+sm.gdd+photo|species), data=test)
+x<-subset(test, test$numinds>=6)
 
-bb<-stan_glmer(budburst~d.index+(d.index|species), data=test)
+dvr.stan<-subset(x, select=c("risk", "d.index", "budburst", "sm.sla", "frz", "species", "sm.chill", 
+                            "force", "photo", "site"))
+dvr.stan<-dvr.stan[!duplicated(dvr.stan),]
+dvr.stan<-na.omit(dvr.stan)
 
-fit2<-stan_glmer(risk~d.index+(1|species), data=test)
+
+## Maybe one model with serrations, bb date and frz and trichomes and another with classic force, chill and photo?
+traits<-stan_glm(risk ~ d.index + budburst + frz + (d.index + budburst + frz|species), data=dvr.stan, chains=2)
+
+dvr<-brm(budburst~sm.chill+force+photo+sm.chill:force+sm.chill:photo+force:photo +
+                  (sm.chill+force+photo|species), data=dvr.stan, chains=4, control = list(max_treedepth = 12))
+
+phen<-as.data.frame(tidy(dvr,robust = TRUE))
+
+bb<-stan_glmer(budburst~d.index+(d.index|species), data=dvr.stan)
+
+fit2<-stan_glmer(risk~d.index+(1|species), data=dvr.stan)
 
 library(brms)
-fit.brm<-brm(risk~d.index+budburst+(d.index+budburst|species), data=test)
+fit.brm<-brm(risk~d.index+budburst+(d.index+budburst|species), data=dvr.stan)
 
 
 
