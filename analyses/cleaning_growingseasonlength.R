5ced6de16a12c0b9d1688afd608f6018874728a0
cchambe12 <cchamberlain@g.harvard.edu>
2019-07-26 11:25:02 -0400

    more data

--- analyses/cleaning_growingseasonlength.R
+++ analyses/cleaning_growingseasonlength.R
@@ -28,9 +28,7 @@ df<-na.omit(df)
 df$date<-substr(df$date, 2,8)
 df$date<-as.character(as.Date(df$date,"%m.%d.%y"))
 df$doy<-yday(df$date)
-df$species<-substr(df$Ind, 0,6)
 df<-dplyr::select(df, -date)
-df$species<-ifelse(df$species=="betpap", "BETPAP", df$species)
 df$bbch<-gsub(",", " ", df$bbch, fixed=TRUE)
 
 
@@ -38,16 +36,14 @@ df<-df[!(df$bbch==""),]
 dx<-separate(df, bbch, into = c("first", "second"), sep = " (?=[^ ]+$)")
 dx<-separate(dx, first, into = c("first", "third"), sep = " (?=[^ ]+$)")
 
-dx$Ind <- ifelse(dx$Ind=="[SORAME09_GR]", "SORAME09_GR", dx$Ind)
-dx$Ind <- ifelse(dx$Ind=="BETPOP03_H+A+9:9", "BETPOP03_HF", dx$Ind)
 
 dx$eos<-NA
 dx$eos<-ifelse(dx$first=="99" | dx$first=="102" | dx$second=="99" | dx$second=="102" | dx$third=="99" | dx$third=="102", dx$doy, dx$eos)
 
 dx <- dx[!is.na(dx$eos),]
-dx$idnum <- substr(dx$Ind, 7,8)
-dx$site <- substr(dx$Ind, 10,11)
+dx$Individual <- dx$Ind
 
+deos <- subset(dx, select=c("Individual", "Plot", "eos"))
 
-### Need to clean up individual names first before finding season length for each individual!
+#write.csv(deos, file="output/cg_endofseason.csv", row.names=FALSE)
 
